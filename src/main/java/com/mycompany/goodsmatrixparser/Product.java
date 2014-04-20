3873e3677521184f5622843c1e4c25c1aa301a2b
JustFox <justfox2@gmail.com>
2014-04-20 14:22:50 +0400

    first version

---
+++ src/main/java/com/mycompany/goodsmatrixparser/Product.java
@@ -0,0 +1,43 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package com.mycompany.goodsmatrixparser;
+
+/**
+ *
+ * @author hp
+ */
+public class Product {
+    private String name;
+    private String composition;
+    private String foodEnergy;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getComposition() {
+        return composition;
+    }
+
+    public void setComposition(String composition) {
+        this.composition = composition;
+    }
+
+    public String getFoodEnergy() {
+        return foodEnergy;
+    }
+
+    public void setFoodEnergy(String foodEnergy) {
+        this.foodEnergy = foodEnergy;
+    }
+    
+    
+}
