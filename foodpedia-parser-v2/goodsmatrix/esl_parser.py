1332d6bbd1b3774c04dd93b5d645cb4a0f9db706
Alexander Chistyakov <alexander.chistyakov@f-secure.com>
2014-11-27 22:46:32 +0300

    extended supported ways to define ESLs and test coverage for regex

--- foodpedia-parser-v2/goodsmatrix/esl_parser.py
+++ foodpedia-parser-v2/goodsmatrix/esl_parser.py
@@ -4,19 +4,18 @@ import re
 
 LIST_OF_POSSIBLE_ESL = {
         'proteins': u'Белки|белки',
-        'fats': u'Жиры',
-        'carbohydrates': u'Углеводы',
-        'calories': u'Энергетическая ценность'}
+        'fats': u'Жиры|жиры',
+        'carbohydrates': u'Углеводы|углеводы',
+        'calories': u'Энергетическая\ ценность|энергетическая\ ценность'}
 
 
 def parse_esl(string):
     parsed_dict = dict()
     for key in LIST_OF_POSSIBLE_ESL:
-        m = re.search(ur"({0})\s*:\s*(?P<weight>\d*[,|\.]?\d+)\s*г?".format(LIST_OF_POSSIBLE_ESL[key]),
+        m = re.search(ur"({0})\s*:\s*(?P<weight>\d*[,|\.]?\d+)\s*[г|ккал]?".format(LIST_OF_POSSIBLE_ESL[key]),
                       string,
                       flags=re.IGNORECASE)
         if m:
-            #print m.group('weight')
             number = float(m.group('weight').replace(u',', u'.'))
             parsed_dict[key] = number
     return parsed_dict
--- foodpedia-parser-v2/goodsmatrix/test/test_esl_parser.py
+++ foodpedia-parser-v2/goodsmatrix/test/test_esl_parser.py
@@ -7,38 +7,46 @@ from goodsmatrix.esl_parser import parse_esl
 
 class TestEslParser(unittest.TestCase):
     def test_signle_protein(self):
-        self.assertEqual(parse_esl(u"Белки: 7,90 г"), {'proteins': 7.90})
+        self.assertEqual(parse_esl(u"Белки: 1,23 г"), {'proteins': 1.23})
 
-    def test_protein_in_lower_case(self):
-        self.assertEqual(parse_esl(u"белки: 7,90 г"), {'proteins': 7.90})
+    def test_fats_in_lower_case(self):
+        self.assertEqual(parse_esl(u"жиры: 4,56 г"), {'fats': 4.56})
 
-    def test_protein_no_measuarment(self):
-        self.assertEqual(parse_esl(u"Белки: 7,90"), {'proteins': 7.90})
+    def test_carbohydrates_no_measuarment(self):
+        self.assertEqual(parse_esl(u"Углеводы: 8,90"), {'carbohydrates': 8.90})
+
+    def test_point_in_number(self):
+        self.assertEqual(parse_esl(u"Энергетическая ценность: 123.0 г"), {'calories': 123.0})
 
     def test_protein_no_spaces(self):
         self.assertEqual(parse_esl(u"Белки:7,90г"), {'proteins': 7.90})
 
-    def test_protein_doubled_spaces(self):
-        self.assertEqual(parse_esl(u"Белки  :  7,90  г"), {'proteins': 7.90})
+    def test_fats_doubled_spaces(self):
+        self.assertEqual(parse_esl(u"Жиры  :  1,2  г"), {'fats': 1.2})
 
     def test_empty_string(self):
         self.assertEqual(parse_esl(u""), dict())
 
-    def test_point_in_number(self):
-        self.assertEqual(parse_esl(u"Белки: 7.90 г"), {'proteins': 7.90})
-
     def test_started_with_spaces(self):
-        self.assertEqual(parse_esl(u"   Белки: 7.90 г"), {'proteins': 7.90})
+        self.assertEqual(parse_esl(u"   Углеводы: 8 г"), {'carbohydrates': 8.0})
 
     def test_number_is_missed(self):
-        self.assertEqual(parse_esl(u"Белки: г"), dict())
+        self.assertEqual(parse_esl(u"Энергетическая ценность г"), dict())
 
     def test_trailing_dot(self):
-        self.assertEqual(parse_esl(u"Белки: 7,90 г."), {'proteins': 7.90})
+        self.assertEqual(parse_esl(u"Белки: 7.0 г."), {'proteins': 7.0})
 
-    def test_complete(self):
+    def test_proteins_and_fats_together(self):
+        self.assertEqual(parse_esl(u"Жиры: 1,7 белки :  5 г."), {'proteins': 5.0, 'fats': 1.7})
+
+    def test_carbohydrates_and_calories_together(self):
         self.assertEqual(
-            parse_esl(u"Белки:  7,50 г Жиры:  2,90 г Углеводы:  3,00 г Энергетическая ценность:  263,00 ккал: 7,90 г."),
+            parse_esl(u"  углеводы: 3 ; Энергетическая ценность : 1000 ккал"),
+            {'carbohydrates': 3.0, 'calories': 1000})
+
+    def test_all_esl_together(self):
+        self.assertEqual(
+            parse_esl(u"Белки:  7,50 г Жиры:  2,90 г Углеводы:  3,00 г Энергетическая ценность:  263,00 ккал"),
             {'proteins': 7.50, 'fats': 2.9, 'carbohydrates': 3.0, 'calories': 263.0})
 
 
