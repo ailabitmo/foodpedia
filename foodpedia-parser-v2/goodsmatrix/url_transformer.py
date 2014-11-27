1d9ff657fd6ef761c193592db383440cbd92e4f7
Alexander Chistyakov <alexander.chistyakov@f-secure.com>
2014-11-27 23:36:17 +0300

    added description for url transformers

--- foodpedia-parser-v2/goodsmatrix/url_transformer.py
+++ foodpedia-parser-v2/goodsmatrix/url_transformer.py
@@ -1,6 +1,13 @@
+"""utility functions to transforms goodmatrix.ru's specific urls"""
 def transform_catalog_node_url_to_url_with_goods(catalog_node_url):
+    """transofrm catalog's end node's url with pagination
+    to a url with a single-page list of products for the category.
+    """
     return catalog_node_url.replace('goods-catalogue', 'map')
 
 
 def transform_goods_analogues_url_to_goods_url(goods_analogues_url):
+    """transform url with a list of analogues for a good
+    to a url with description for the good
+    """
     return goods_analogues_url.replace("goods-analogue", "goods")
