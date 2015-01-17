e93d221c85688b001bb81c469d7928df889730d5
Alexander Chistyakov <alexander.chistyakov@f-secure.com>
2015-01-17 14:54:31 +0300

    improved building of pubby's config file

--- foodpedia-pubby/pubby/run_pubby_on_tomcat.sh
+++ foodpedia-pubby/pubby/run_pubby_on_tomcat.sh
@@ -4,21 +4,26 @@ if [ -z "$1" ]
 then
   echo "url of project's webbase is not specified as command line argument">&2
   exit 1
+else
+  homeURL=$1
 fi
 
 if [ -z "$2" ]
 then
   if [ -z "$ENDPOINT_PORT_8890_TCP_ADDR" ]
   then
-    sparql_endpoint_url="http://foodpedia.tk/sparql"
-    echo "A container with an alias 'endopoint' is not linked. Use the '$sparql_endpoint_url' as endpoint's url">&2
+    sparqlEndpointURL="http://foodpedia.tk/sparql"
+    echo "A container with an alias 'endopoint' is not linked. Use the '$sparqlEndpointURL' as endpoint's url">&2
   else
-    sparql_endpoint_url="http://endpoint:8890/sparql"
+    sparqlEndpointURL="http://endpoint:8890/sparql"
   fi
 else
-  sparql_endpoint_url=$2
+  sparqlEndpointURL=$2
 fi
+export homeURL sparqlEndpointURL
 
-chmod +x /usr/local/form_configfile.sh
-/bin/bash /usr/local/form_configfile.sh "$1" "$sparql_endpoint_url"
+WEBAPP_HOME=/usr/local/tomcat/webapps/ROOT/WEB-INF
+envsubst <$WEBAPP_HOME/config.ttl | sponge $WEBAPP_HOME/config.ttl
+envsubst <$WEBAPP_HOME/prefixes.n3 | sponge $WEBAPP_HOME/prefixes.n3
+
 catalina.sh run
