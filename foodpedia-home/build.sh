2d7df5358601006c3de19718867d942aef7b2402
Alexander Chistyakov <alexander.chistyakov@f-secure.com>
2015-01-26 04:23:09 +0300

    cosmetic changes in build.sh

--- foodpedia-home/build.sh
+++ foodpedia-home/build.sh
@@ -2,16 +2,20 @@
 
 if [ -z "$1" ]
 then
-      profile_name=development
-        echo "a name of maven profile (development|testing|production) is not specified as command line argument.">&2
-          echo "Use default $profile_name">&2
-      else
-            profile_name=$1
-        fi
+  profile_name=development
+  echo "a name of maven profile (development|testing|production) is not specified as command line argument.">&2
+  echo "Use default $profile_name">&2
+else
+  profile_name=$1
+fi
 
-        POM_DIRECTORY_PATH=$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )
-        RESULT_WAR_NAME=foodpedia-home-$profile_name.war
+POM_DIRECTORY_PATH=$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )
+RESULT_WAR_NAME=foodpedia-home-$profile_name.war
 
-    docker run -it --rm --name home_builder -v $POM_DIRECTORY_PATH:/usr/src/mymaven -w /usr/src/mymaven maven:3.2-jdk-7 mvn clean install -P $profile_name
-    cp $POM_DIRECTORY_PATH/target/foodpedia-home-1.0-SNAPSHOT.war $POM_DIRECTORY_PATH/$RESULT_WAR_NAME
-
+if [ -z "$M2_REPO" ]
+then
+    docker run -it --rm --name home_builder -v $POM_DIRECTORY_PATH:/usr/src/mymaven -w /usr/src/mymaven maven:3.2-jdk-7 mvn clean package -P $profile_name
+else
+    docker run -it --rm --name home_builder -v $POM_DIRECTORY_PATH:/usr/src/mymaven -v $M2_REPO:/root/.m2/repository/ -w /usr/src/mymaven maven:3.2-jdk-7 mvn clean package -P $profile_name
+fi
+cp $POM_DIRECTORY_PATH/target/foodpedia-home-1.0-SNAPSHOT.war $POM_DIRECTORY_PATH/$RESULT_WAR_NAME
