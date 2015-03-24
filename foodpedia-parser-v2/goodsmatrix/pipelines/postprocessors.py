b52547333e2bf480745a4856fad09556fb369a27
Alexander Chistyakov <alexander.chistyakov@f-secure.com>
2015-03-24 09:48:24 +0300

    fixed parsing of items without ingredients

--- foodpedia-parser-v2/goodsmatrix/pipelines/postprocessors.py
+++ foodpedia-parser-v2/goodsmatrix/pipelines/postprocessors.py
@@ -59,23 +59,24 @@ class ExtractIngredients(object):
         self.not_parsed_fragments = dict()
 
     def process_item(self, good_item, spider):
-        ingredients_as_string = good_item['ingredients']
-        ingredients_as_string = string_processor.remove_substring_in_paranthesis(
-            ingredients_as_string)
-        ingredients_fragments = string_processor.split_ingredients(ingredients_as_string)
-        #log.msg("ingredients after splitting: {0}".format(ingredients_fragments))
-        for fragment in ingredients_fragments:
-            if not string_processor.parse_e_additives(fragment):
-                fragment = string_processor.remove_weight(fragment)
-                fragment = string_processor.remove_percents(fragment)
-                agrovoc_match = self.agrovoc_graph.find_ingredient_by_name(fragment.strip())
-                if agrovoc_match:
-                    #log.msg('found ingredient {0}'.format(agrovoc_match))
-                    good_item['agrovoc_ingredients'] = (
-                        good_item.get('agrovoc_ingredients', []) + [agrovoc_match])
-                else:
-                    self.not_parsed_fragments[fragment] = (
-                        self.not_parsed_fragments.get(fragment, 0) + 1)
+        if 'ingredients' in good_item:
+            ingredients_as_string = good_item['ingredients']
+            ingredients_as_string = string_processor.remove_substring_in_paranthesis(
+                ingredients_as_string)
+            ingredients_fragments = string_processor.split_ingredients(ingredients_as_string)
+            #log.msg("ingredients after splitting: {0}".format(ingredients_fragments))
+            for fragment in ingredients_fragments:
+                if not string_processor.parse_e_additives(fragment):
+                    fragment = string_processor.remove_weight(fragment)
+                    fragment = string_processor.remove_percents(fragment)
+                    agrovoc_match = self.agrovoc_graph.find_ingredient_by_name(fragment.strip())
+                    if agrovoc_match:
+                        #log.msg('found ingredient {0}'.format(agrovoc_match))
+                        good_item['agrovoc_ingredients'] = (
+                            good_item.get('agrovoc_ingredients', []) + [agrovoc_match])
+                    else:
+                        self.not_parsed_fragments[fragment] = (
+                            self.not_parsed_fragments.get(fragment, 0) + 1)
         return good_item
 
     def close_spider(self, spider):
