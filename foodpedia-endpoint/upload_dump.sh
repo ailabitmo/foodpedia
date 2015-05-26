60be6ebe61331740beada7cf886cc025423647e4
Alexander Chistyakov <alexander.chistyakov@f-secure.com>
2015-05-26 12:51:36 +0300

    added Virtuoso reindexing after dump's uploading to make bif:contains work

--- foodpedia-endpoint/upload_dump.sh
+++ foodpedia-endpoint/upload_dump.sh
@@ -8,6 +8,12 @@ if [ "$(ls -A $DIR_WITH_DATA_TO_UPLOAD)" ]
 then
   isql-vt $SERVER_PORT $USERNAME $PASSWORD exec="ld_dir_all('$DIR_WITH_DATA_TO_UPLOAD', '*', '$GRAPH_NAME');"
   isql-vt $SERVER_PORT $USERNAME $PASSWORD exec="rdf_loader_run();" 
+  #Reindexing. Needed for Virtuoso 6
+  #see https://github.com/openlink/virtuoso-opensource/issues/107
+  isql-vt $SERVER_PORT $USERNAME $PASSWORD exec="DB.DBA.RDF_OBJ_FT_RULE_ADD (null, null, 'All');" 
+  isql-vt $SERVER_PORT $USERNAME $PASSWORD exec="DB.DBA.VT_INC_INDEX_DB_DBA_RDF_OBJ ();"
+  isql-vt $SERVER_PORT $USERNAME $PASSWORD exec="DB.DBA.VT_INDEX_DB_DBA_RDF_OBJ ();"
+
   #wait 
   #isql-vt 1111 dba dba exec="checkpoint;" 
 else
